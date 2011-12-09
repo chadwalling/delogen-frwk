12bd5353ecd65c2cd0f0788b34817d8c3fbc505c
Chad Walling <chadwalling@gmail.com>
2011-12-09 16:49:51 -0700

    Update frwk/models/server/Session.php

--- frwk/models/server/Session.php
+++ frwk/models/server/Session.php
@@ -6,25 +6,25 @@ Render::Model('data/Data.php');
 
 class Session extends Data
 {
-    var $id = '';
-    var $file = '';
-    function __construct()
-    {
-      $this->init();
-    }
-
-    function init()
-    {
-        session_start();
-    }
-
-    function isStarted()
-    {
+	var $id = '';
+	var $file = '';
+	function __construct()
+	{
+	$this->init();
+	}
+	
+	function init()
+	{
+	session_start();
+	}
+	
+	function isStarted()
+	{
 	    return session_id();
 	    return Session::getid();
-    }
+	}
 
-    //creates a session cookie ... assumes authentication was successful
+    	//creates a session cookie ... assumes authentication was successful
 	function _makeid($class_id)
 	{
 		$ip = $_SERVER["REMOTE_ADDR"];
@@ -37,28 +37,28 @@ class Session extends Data
 		$this->id = session_id();
 		$salt = $class_id.$this->id.microtime().$ip;
 		$_SESSION['id'] = $salt;
-        	$_SESSION['class_id'] = $class_id;
+		$_SESSION['class_id'] = $class_id;
 		$hash = hash( 'sha256', $salt.$class_id);
-
+	
 		$sess = new Objects("sessions");
 		$date = date('Y-m-d H:i:s');
-        	$expiresDate = date('Y-m-d H:i:s', strtotime($date ."+ 1 hour"));
-
+		$expiresDate = date('Y-m-d H:i:s', strtotime($date ."+ 1 hour"));
+	
 		$sess->addRow(array($this->id, $class_id, $hash, $date, $expiresDate, $date));
 		$sess->insert(true);
-
+	
 		return $this->id;
 	}
 
-    function getLoginType(){
-        if(session_id() !== "" && !empty($_SERVER['class_id']) ){
-            $logins = new Objects("logins");
-            $logins->setclass_id($_SERVER['class_id']);
-            $l = $logins->fetch();
-            return $l->getlogintype_id();
-        }
-        return NULL;
-    }
+	function getLoginType(){
+		if(session_id() !== "" && !empty($_SERVER['class_id']) ){
+		    $logins = new Objects("logins");
+		    $logins->setclass_id($_SERVER['class_id']);
+		    $l = $logins->fetch();
+		    return $l->getlogintype_id();
+		}
+		return NULL;
+	}
     
 	function getid(){
 		if(Session::isAuth()){
@@ -69,18 +69,18 @@ class Session extends Data
 
 	function isAuth()
 	{
-        if(session_id() == "") return false;
+	        if(session_id() == "") return false;
 		$sessObject = new Objects("sessions");
 		$sessObject->setSession_id(session_id());
 		$newsess = $sessObject->fetch();
 		$hash = $newsess->getHash();
 		$class_id = $newsess->getclass_id();
-        if($_SESSION['id'] && $class_id){
-		    if($hash == hash( 'sha256', $_SESSION['id'].$class_id)){
+	        if($_SESSION['id'] && $class_id){
+			if($hash == hash( 'sha256', $_SESSION['id'].$class_id)){
 			    Session::_makeid($class_id);
 			    return true;
-		    }
-        }
+			}
+	        }
 		return false;
 	}
 
@@ -98,7 +98,7 @@ class Session extends Data
 		$file = $this->file;
 		preg_match('/\/home\/(.*)?/', $_SERVER['DOCUMENT_ROOT'], $match );
 		$site = $match[1];
-        //error_log(/tmp/cache/".$site."/".$file);
+        	//error_log(/tmp/cache/".$site."/".$file);
 		return CACHE_DIR.$site."/".$file;
 	}
 }
